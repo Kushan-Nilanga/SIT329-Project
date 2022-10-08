204657e5fef34b39f1a443f3b4da202969f033bb
SachiniWitharana <53445675+SachiniWitharana@users.noreply.github.com>
2022-10-08 17:31:46 +1100

    Update position_detect.py

--- position_detect.py
+++ position_detect.py
@@ -18,29 +18,6 @@ def on_connect(client, userdata, flags, rc):
         print(f"Connected fail with code {rc}")
 Position = ["topTube", "Pantani" ,"Froome", "Elbows"]
 
-# #GPIO Mode (BOARD / BCM)
-# GPIO.setmode(GPIO.BCM)
-# #set GPIO Pins
-# GPIO_TRIGGER = 23
-# GPIO_ECHO = 24
-# #set GPIO direction (IN / OUT)
-# GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
-# GPIO.setup(GPIO_ECHO, GPIO.IN)
-
-# def distance():
-#     GPIO.output(GPIO_TRIGGER, True)
-#     time.sleep(0.00001)
-#     GPIO.output(GPIO_TRIGGER, False)
-#     StartTime = time.time()
-#     StopTime = time.time()
-#     while GPIO.input(GPIO_ECHO) == 0:
-#         StartTime = time.time()
-#     while GPIO.input(GPIO_ECHO) == 1:
-#         StopTime = time.time()
-#     TimeElapsed = StopTime - StartTime
-#     distance = (TimeElapsed * 34300) / 2
-#     return distance
-
 drops_sensor = random.randint(1, 2)
 hoods_sensor = 0
 front_sensor = random.randint(1, 2)
